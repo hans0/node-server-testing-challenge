d327cbd6b4f7cb3ad62f93d0f8fcd9136b3d6248
hans0 <kiessler.hans@gmail.com>
2021-05-20 16:59:37 -0400

    Got getSurvivorById's structure where I want it'

--- api/survivors/model.js
+++ api/survivors/model.js
@@ -13,17 +13,16 @@ async function getSurvivorById(id) {
     .where('survivor_id', id)
     .first()
   const perks = await db('unique-survivor-perks as usp')
-    .select('usp.perk_id')
+    .leftJoin('perks as p', 'p.perk_id', 'usp.perk_id')
+    .select('usp.perk_id', 'p.perk_name')
     .where('usp.survivor_id', id)
-    // .leftJoin('perks as p')
-    // .where('p.perk_id', 'usp.perk_id')
-    // .select('p.perk_id', 'p.perk_name')
-  // return await db('survivors as s')
-  //   .leftJoin('unique-survivor-perks as usp', 's.survivor_id', 'usp.survivor_id')
-  //   .select('s.survivor_id', 's.survivor_name', 'usp.perk_id')
-  //   .where('s.survivor_id', id)
+    
+  
   console.log(perks)
-  return survivor;
+  return { 
+    ...survivor,
+    perks: perks
+  };
 }
 
 
